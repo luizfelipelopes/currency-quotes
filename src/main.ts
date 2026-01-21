d5e5433218726670835b593220eaf48fd92abcfc
Luiz Felipe <lfelipelopesti@gmail.com>
2026-01-20 21:13:14 -0300

    feat: add enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.BASE_URL_FRONTEND,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
